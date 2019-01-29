f2c8c2662f960bf6ea9599b662481426aad56e53
Xenophilicy <Xenophilicy@users.noreply.github.com>
2019-01-28 23:14:07 -0600

    Update v1.5.0
    Added support for unlimited ranks

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
 # MakeMeAdmin
+[![](https://poggit.pmmp.io/shield.state/MakeMeAdmin)](https://poggit.pmmp.io/p/MakeMeAdmin)
 [![HitCount](http://hits.dwyl.io/Xenophilicy/MakeMeAdmin.svg)](http://hits.dwyl.io/Xenophilicy/MakeMeAdmin)
 ![](https://img.shields.io/discord/490677165289897995.svg?style=flat-square)
 
 # [![Xenophilicy](https://i.imgur.com/KIxx8cS.png)]()
 
 ## Information
-With this plugin, you can very easily switch your group from **PurePerms** with a simple command that pops a UI onto your screen thanks to integration of **FormAPI**! All you do from there is select which group you want to switch to, and voila! This plugin also uses permissions to determine which groups should popup for which players, depending on which nodes are added to your group or player file. The ones not available, show as "Locked" and are grayed out in the UI.
+With this plugin, you can very easily switch your group from **PurePerms** with a simple command that pops a UI onto your screen thanks to integration of **FormAPI**! All you do from there is select which group you want to switch to, and voila! This plugin also uses permission nodes to determine which groups should popup for which players, depending on which nodes are added to your group or player file. The ones not available, show as "Locked" and are grayed out in the UI. Each rank has an assigned permission node that can be edited in the config.yml.
 
 *You can find a video example on YouTube → https://www.youtube.com/watch?v=3O9LJUtYLi8*
 
 ## Commands
 ```diff
-/mma or /mmadmin → Get the form list of groups!
-/makemeadmin → Get info about the plugin!
+/mma → Get the form list of groups!
 ```
 ***
 
 ## MakeMeAdmin Details
 * **API:** 3.5.0+
-* **Version:** 1.2.2
+* **Version:** 1.5.0
 * **Basic Description:** Easily switch ranks with a simple command and a click of the mouse!
 * *Easy to edit config.yml file*
 * *Simple code for editing and debugging*
@@ -30,5 +30,4 @@ With this plugin, you can very easily switch your group from **PurePerms** with 
 **DevTools → https://github.com/pmmp/PocketMine-DevTools** *(If you are using the plugin folder method)*
 
 ## Credits
-* [「oKqzan」](https://github.com/oKqzan/)
 * [Xenophilicy](https://github.com/Xenophilicy/)
--- src/Xenophilicy/MakeMeAdmin/MakeMeAdmin.php
+++ src/Xenophilicy/MakeMeAdmin/MakeMeAdmin.php
@@ -11,31 +11,18 @@
 # $$/   $$/  $$$$$$$/ $$/   $$/  $$$$$$/  $$$$$$$/  $$/   $$/ $$/ $$/ $$/  $$$$$$$/  $$$$$$$ |
 #                                         $$ |                                      /  \__$$ |
 #                                         $$ |                                      $$    $$/ 
-#                                         $$/                                        $$$$$$/           
-# EDITED BY:
-#
-#            /$$   /$$                                        
-#           | $$  /$$/                                        
-#   /$$$$$$ | $$ /$$/   /$$$$$$  /$$$$$$$$  /$$$$$$  /$$$$$$$ 
-#  /$$__  $$| $$$$$/   /$$__  $$|____ /$$/ |____  $$| $$__  $$
-# | $$  \ $$| $$  $$  | $$  \ $$   /$$$$/   /$$$$$$$| $$  \ $$
-# | $$  | $$| $$\  $$ | $$  | $$  /$$__/   /$$__  $$| $$  | $$
-# |  $$$$$$/| $$ \  $$|  $$$$$$$ /$$$$$$$$|  $$$$$$$| $$  | $$
-#  \______/ |__/  \__/ \____  $$|________/ \_______/|__/  |__/
-#                           | $$                              
-#                           | $$                              
-#                           |__/                              
+#                                         $$/                                        $$$$$$/                       
 
 namespace Xenophilicy\MakeMeAdmin;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\command\{Command,CommandSender,CommandExecuter,ConsoleCommandSender};
+use pocketmine\command\{Command,CommandSender,ConsoleCommandSender};
 use pocketmine\utils\config;
 use pocketmine\scheduler\PluginTask;
 use pocketmine\{Server,Player};
 
-use jojoe77777\FormAPI\SimpleForm;
+use Xenophilicy\MakeMeAdmin\libs\jojoe77777\FormAPI\SimpleForm;
 
 class MakeMeAdmin extends PluginBase implements Listener{
 
@@ -46,6 +33,7 @@ class MakeMeAdmin extends PluginBase implements Listener{
         $this->saveDefaultConfig();
         $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
         $this->config->getAll();
+        $this->ranks = $this->config->get("Ranks");
         $this->getLogger()->info("MakeMeAdmin has been enabled!");
         $pureinstalled = $this->getServer()->getPluginManager()->getPlugin("PurePerms");
         if($pureinstalled == null){
@@ -55,161 +43,65 @@ class MakeMeAdmin extends PluginBase implements Listener{
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-        switch($command->getName()){
-            case'makemeadmin':
-                $sender->sendMessage("§7-=== §6MakeMeAdmin §7===-");
-                $sender->sendMessage("§eAuthor: §aXenophillicy");
-                $sender->sendMessage("§eDescription: §aEasily change ranks with a command!");
-                $sender->sendMessage("§7-====================-");
-                break;
-            case'mma'||'mmadmin':
-                if($sender->hasPermission("makemeadmin.use.ui")) {
-                    if ($sender instanceof Player){
-                        $this->rankOptions($sender);
-                    }
-                    else {
-                        $sender->sendMessage("§cThis is an in-game command only!");
-                    }
+        if($command->getName() == 'mma'){
+            if($sender->hasPermission("makemeadmin.use.ui")) {
+                if ($sender instanceof Player){
+                    $this->rankOptions($sender);
                 }
                 else {
-                    $sender->sendMessage("§cYou don't have permission to switch ranks!");
+                    $sender->sendMessage("§cThis is an in-game command only!");
                 }
-                break;
+            }
+            else {
+                $sender->sendMessage("§cYou don't have permission to switch ranks!");
+            }
         }
         return true;
     }
 
-    public function removeColor($string){
-        $string = str_replace('§0', '', $string);
-        $string = str_replace('§1', '', $string);
-        $string = str_replace('§2', '', $string);
-        $string = str_replace('§3', '', $string);
-        $string = str_replace('§4', '', $string);
-        $string = str_replace('§5', '', $string);
-        $string = str_replace('§6', '', $string);
-        $string = str_replace('§7', '', $string);
-        $string = str_replace('§8', '', $string);
-        $string = str_replace('§9', '', $string);
-        $string = str_replace('§a', '', $string);
-        $string = str_replace('§b', '', $string);
-        $string = str_replace('§c', '', $string);
-        $string = str_replace('§d', '', $string);
-        $string = str_replace('§e', '', $string);
-        $string = str_replace('§f', '', $string);
-        $string = str_replace('§k', '', $string);
-        $string = str_replace('§l', '', $string);
-        $string = str_replace('§m', '', $string);
-        $string = str_replace('§n', '', $string);
-        $string = str_replace('§o', '', $string);
-        $string = str_replace('§r', '', $string);
-        return $string;
-    }
-
     public function rankOptions($player){
-        $formapi = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
         $form = new SimpleForm(function (Player $player, $data){
-            if($data === null){
+            if ($data === null){
                 return;
             }
-            $prefix = $this->config->get("group_command");
-            $sender = $player->getName();
-            $consolecmd = new ConsoleCommandSender();
-            switch($data){
-                case 0:
-                    if($player->hasPermission("makemeadmin.use.1")){
-                        $this->getServer()->getCommandMap()->dispatch($consolecmd, $prefix.' '.$sender.' '.$this->config->getNested("rank1.alias"));
-                        $player->sendMessage("§aYou selected §e".$this->config->getNested("rank1.label")."§r§a as your rank!");
-                        $this->getLogger()->notice("§e".$sender." has changed their group to ".$this->config->getNested("rank1.label"));  
-                    }
-                    else{
-                        $player->sendMessage("§cYou don't have permission to switch to this rank!");
-                    }
-                    break;
-                case 1:
-                    if($player->hasPermission("makemeadmin.use.2")){
-                        $this->getServer()->getCommandMap()->dispatch($consolecmd, $prefix.' '.$sender.' '.$this->config->getNested("rank2.alias"));
-                        $player->sendMessage("§aYou selected §e".$this->config->getNested("rank2.label")."§r§a as your rank!");
-                        $this->getLogger()->notice("§e".$sender." has changed their group to ".$this->config->getNested("rank2.label"));  
-                    }
-                    else{
-                        $player->sendMessage("§cYou don't have permission to switch to this rank!");
-                    }
-                    break;
-                case 2:
-                    if($player->hasPermission("makemeadmin.use.3")){
-                        $this->getServer()->getCommandMap()->dispatch($consolecmd, $prefix.' '.$sender.' '.$this->config->getNested("rank3.alias"));
-                        $player->sendMessage("§aYou selected §e".$this->config->getNested("rank3.label")."§r§a as your rank!");
-                        $this->getLogger()->notice("§e".$sender." has changed their group to ".$this->config->getNested("rank3.label"));  
-                    }
-                    else{
-                        $player->sendMessage("§cYou don't have permission to switch to this rank!");
-                    }
-                    break;
-                case 3:
-                    if($player->hasPermission("makemeadmin.use.4")){
-                        $this->getServer()->getCommandMap()->dispatch($consolecmd, $prefix.' '.$sender.' '.$this->config->getNested("rank4.alias"));
-                        $player->sendMessage("§aYou selected §e".$this->config->getNested("rank4.label")."§r§a as your rank!");
-                        $this->getLogger()->notice("§e".$sender." has changed their group to ".$this->config->getNested("rank4.label"));  
-                    }
-                    else{
-                        $player->sendMessage("§cYou don't have permission to switch to this rank!");
-                    }
-                    break;
-                case 4:
-                    if($player->hasPermission("makemeadmin.use.5")){
-                        $this->getServer()->getCommandMap()->dispatch($consolecmd, $prefix.' '.$sender.' '.$this->config->getNested("rank5.alias"));
-                        $player->sendMessage("§aYou selected §e".$this->config->getNested("rank5.label")."§r§a as your rank!");
-                        $this->getLogger()->notice("§e".$sender." has changed their group to ".$this->config->getNested("rank5.label"));  
-                    }
-                    else{
-                        $player->sendMessage("§cYou don't have permission to switch to this rank!");
-                    }
-                    break;
+            else{
+                $prefix = $this->config->get("Group_Command");
+                $name = $player->getName();
+                $consolecmd = new ConsoleCommandSender();
+                $value = explode(":", $this->ranks[$data]);
+                $value = str_replace("&", "§", $value);
+                if($player->hasPermission($value[2])){
+                    $this->getServer()->getCommandMap()->dispatch($consolecmd, $prefix.' '.$name.' '.$value[1]);
+                    $player->sendMessage("§aYou selected §e".$value[0]."§r§a as your rank!");
+                    $this->getLogger()->notice("§e".$name." has changed their group to ".$value[0]);  
+                }
+                else{
+                    $player->sendMessage("§cYou don't have permission to switch to this rank!");
+                    return;
+                }
             }
             return true;
         });
         $form->setTitle("§6Server Ranks");
         $form->setContent("§aPick the rank to switch to!");
-        $label1 = $this->config->getNested("rank1.label");
-        $label2 = $this->config->getNested("rank2.label");
-        $label3 = $this->config->getNested("rank3.label");
-        $label4 = $this->config->getNested("rank4.label");
-        $label5 = $this->config->getNested("rank5.label");
-        if($player->hasPermission("makemeadmin.use.1")){
-            $form->addButton($label1);
-        }
-        else{
-            $label1 = $this->removeColor($label1);
-            $form->addButton("§r§8".$label1." (Locked)");
-        }
-        if($player->hasPermission("makemeadmin.use.2")){
-            $form->addButton($label2);
-        }
-        else{
-            $label2 = $this->removeColor($label2);
-            $form->addButton("§r§8".$label2." (Locked)");
-        }
-        if($player->hasPermission("makemeadmin.use.3")){
-            $form->addButton($label3);
-        }
-        else{
-            $label3 = $this->removeColor($label3);
-            $form->addButton("§r§8".$label3." (Locked)");
-        }
-        if($player->hasPermission("makemeadmin.use.4")){
-            $form->addButton($label4);
-        }
-        else{
-            $label4 = $this->removeColor($label4);
-            $form->addButton("§r§8".$label4." (Locked)");
-        }
-        if($player->hasPermission("makemeadmin.use.5")){
-            $form->addButton($label5);
-        }
-        else{
-            $label5 = $this->removeColor($label5);
-            $form->addButton("§r§8".$label5." (Locked)");
+        foreach ($this->ranks as $rank) {
+            $value = explode(":", $rank);
+            $value = str_replace("&", "§", $value);
+            if($player->hasPermission($value[2])){
+                $form->addButton($value[0]);
+            }
+            else{
+                $value = $this->removeColor($value);
+                $form->addButton("§r§8".$value[0]." (Locked)");
+            }
         }
         $form->sendToPlayer($player);
+    }
+
+    public function removeColor($string){
+        foreach (['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','k','l','m','n','o','o'] as $target){
+        $string = str_replace("§".$target, '', $string);
+        }
+        return $string;
     }
 }
