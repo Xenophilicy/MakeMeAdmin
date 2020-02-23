08cf62b47e2da721fe4e776c9f6c9a42839aaf9a
Xeno <xenophilicy@gmail.com>
2020-02-23 02:56:26 -0600

    Update v1.6.0
    Add lots of new features, fix a few bugs, clean code. Add PurePerms direct API connection.

--- README.md
+++ README.md
@@ -3,22 +3,18 @@
 [![HitCount](http://hits.dwyl.io/Xenophilicy/MakeMeAdmin.svg)](http://hits.dwyl.io/Xenophilicy/MakeMeAdmin)
 ![](https://img.shields.io/discord/490677165289897995.svg?style=flat-square)
 
-# [![Xenophilicy](https://i.imgur.com/EBoTc6c.png)]()
+# [![Xenophilicy](https://file.xenoservers.net/Resources/GitHub-Resources/makemeadmin/screenshot.png)]()
 
 ## Information
-With this plugin, you can very easily switch your group from **PurePerms** with a simple command that pops a UI onto your screen thanks to integration of **FormAPI**! All you do from there is select which group you want to switch to, and voila! This plugin also uses permission nodes to determine which groups should popup for which players, depending on which nodes are added to your group or player file. The ones not available, show as "Locked" and are grayed out in the UI. Each rank has an assigned permission node that can be edited in the config.yml.
+With this plugin, you can very easily switch your **PurePerms** group with a simple command that pops a UI onto your screen thanks to integration of **FormAPI**! All you do from there is select which group you want to switch to, and voila! This plugin also uses permission nodes to determine which groups should popup for which players, depending on which nodes are added to your group or player file. The ones not available, show as "Locked" and are grayed out in the UI. Each rank has an assigned permission node that can be edited in the config.yml. The command used to access the UI is also customizable in the config.yml along with a bunch of other cool features and settings!
 
 *You can find a video example on YouTube → https://www.youtube.com/watch?v=3O9LJUtYLi8*
 
-## Commands
-```diff
-/mma → Get the form list of groups!
-```
 ***
 
 ## MakeMeAdmin Details
-* **API:** 3.5.0+
-* **Version:** 1.5.2
+* **API:** 3.0.0+
+* **Version:** 1.6.0
 * **Basic Description:** Easily switch ranks with a simple command and a click of the mouse!
 * *Easy to edit config.yml file*
 * *Simple code for editing and debugging*
--- src/Xenophilicy/MakeMeAdmin/MakeMeAdmin.php
+++ src/Xenophilicy/MakeMeAdmin/MakeMeAdmin.php
@@ -15,99 +15,208 @@
 
 namespace Xenophilicy\MakeMeAdmin;
 
-use pocketmine\command\{Command,CommandSender,ConsoleCommandSender};
-use pocketmine\plugin\PluginBase;
+use pocketmine\command\{PluginCommand,Command,CommandSender,ConsoleCommandSender};
+use pocketmine\utils\{TextFormat as TF, config};
+use pocketmine\plugin\{Plugin,PluginBase};
 use pocketmine\event\Listener;
-use pocketmine\utils\config;
 use pocketmine\Player;
-use pocketmine\utils\TextFormat as TF;
 
 use Xenophilicy\MakeMeAdmin\libs\jojoe77777\FormAPI\SimpleForm;
 
 class MakeMeAdmin extends PluginBase implements Listener{
 
-    private $config;
-
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->saveDefaultConfig();
-        $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
-        $this->config->getAll();
-        $this->ranks = $this->config->get("Ranks");
-        $this->getLogger()->info("MakeMeAdmin has been enabled!");
-        $version = $this->config->get("VERSION");
-        if($version != "1.6.0"){
-            $this->getLogger()->warning("You have updated MakeMeAdmin but have an old config! Please delete your old config for the plugin to function propery!");
+        $configPath = $this->getDataFolder()."config.yml";
+        if(!file_exists($configPath)){
+            $this->getLogger()->critical("It appears that this is the first time you are using MakeMeAdmin! This plugin does not function with the default config.yml, so please edit it to your preferred settings before attempting to use it.");
+            $this->saveDefaultConfig();
+            $config = new Config($configPath, Config::YAML);
+            $config->getAll();
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+            return;
         }
-        foreach($this->ranks as $rank){
-            $value = explode(":", $rank);
-            if(isset($value[3])){
-                switch($value[3]){
-                    case 'url':
-                        break;
-                    case 'path':
+        $this->saveDefaultConfig();
+        $this->config = new Config($configPath, Config::YAML);
+        $this->config->getAll();
+        $version = $this->config->get("VERSION");
+        $this->pluginVersion = $this->getDescription()->getVersion();
+        if($version < "1.6.0"){
+            $this->getLogger()->warning("You have updated MakeMeAdmin to v".$this->pluginVersion." but have a config from v$version! Please delete your old config for new features to be enabled and to prevent unwanted errors! Plugin will remain disabled...");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+            return;
+        }
+        foreach($this->ranks = $this->config->get("Ranks") as $rank){
+            $values = explode(":", $rank);
+            if(isset($values[3])){
+                switch($values[3]){
+                    case 'url' || 'path':
                         break;
                     default:
-                        $this->getLogger()->warning("Invalid image type! Rank: ".$value[0].TF::RESET." Image type: ".$value[3]." not supported. ");
+                        $this->getLogger()->warning("One of the ranks you have added has an invalid image type. Rank: ".$values[0].TF::RESET." Image type: ".$values[3]." not supported.");
                 }
             }
         }
+        $this->cmdName = str_replace("/","",$this->config->getNested("Command.Prefix"));
+        if($this->cmdName == null || $this->cmdName == ""){
+            $this->getLogger()->critical("Invalid command prefix found, disabling plugin...");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+            return;
+        } else{
+            $cmd = new PluginCommand($this->cmdName , $this);
+            $cmd->setDescription($this->config->getNested("Command.Description"));
+            if($this->config->getNested("Command.Permission.Require")){
+                $cmd->setPermission($this->config->getNested("Command.Permission.Node"));
+            }
+            $this->getServer()->getCommandMap()->register("makemeadmin", $cmd, $this->cmdName);
+        }
+        if($manager = $this->config->getNested("Group.Manager") == "PurePerms"){
+            if($this->loadPurePerms()){
+                $this->ppEnabled = true;
+                $this->purePerms = $this->getServer()->getPluginManager()->getPlugin("PurePerms");
+                if(($result = $this->verifyGroups($this->ranks)) === true){
+                    $this->getLogger()->info("Successfully verified ranks and PurePerms installation.");
+                } else{
+                    $this->getLogger()->warning("One of the ranks is not valid. Invalid rank: ".$result);
+                    $this->getServer()->getPluginManager()->disablePlugin($this);
+                    return;
+                }
+            } else{
+                $this->getLogger()->warning("You have selected PurePerms as your group manager but the plugin did not load correctly. Please verify PurePerms is installed and reload the server. Plugin will remain disabled...");
+                $this->getServer()->getPluginManager()->disablePlugin($this);
+                return;
+            }
+        } else{
+            $this->ppEnabled = false;
+            $this->getLogger()->notice("Your group manager $manager, is not fully supported. For best compatibility, please use PurePerms instead.");
+        }
+        $this->lang = $this->config->get("Messages");
     }
-    
-    private function checkOP($user){
-        if($this->config->get("OP-Bypass") == FALSE){
-            return FALSE
-        }else{
-            if($user->isOp()){
-                return TRUE
-            }else{
-                return FALSE
+
+    private function verifyGroups(array $ranks){
+        foreach($ranks as $rank){
+            $values = explode(":", $rank);
+            $group = $this->purePerms->getGroup($values[1]);
+            if($group === null || $group === ""){
+                return $values[1];
             }
         }
+        return true;
     }
-    
-    private function startsWith($haystack, $needle){
-        $length = strlen($needle);
-        return (substr($haystack, 0, $length) === $needle);
+
+    private function getData(){
+        return $this->purePerms->getUserDataMgr();
+    }
+
+    private function loadPurePerms() : bool{
+        if($plugin = $this->getServer()->getPluginManager()->getPlugin("PurePerms") instanceof Plugin){
+            $this->purePerms = $plugin;
+            return true;
+        }
+        return false;
+    }
+
+    private function hasPurePermission(Player $player, $perm) : bool{
+        $group = $this->getData()->getGroup($player);
+        $perms = array_merge($this->getData()->getUserPermissions($player),$group->getGroupPermissions());
+        foreach($perms as $purePerm){
+            if($perm == $purePerm){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function hasPerms($player, $perm) : bool{
+        if($player->isOp()){
+            if($this->config->get("OP-Bypass")){
+                return true;
+            } elseif($this->hasPurePermission($player, $perm)){
+                return true;
+            } else{
+                return false;
+            }
+        } elseif($this->hasPurePermission($player, $perm)){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    private function replacePlaceholders(string $source, string $group, string $name = ""){
+        $source = str_replace("&", "§", $source);
+        $source = str_replace("{player}", $name, $source);
+        $source = str_replace("{group}", $group, $source);
+        return $source;
+    }
+
+    private function setPlayerGroup(Player $player, array $values){
+        $values = str_replace("&", "§", $values);
+        $name = $player->getName();
+        if($this->hasPerms($player, $values[2])){
+            if($this->ppEnabled){
+                $group = $this->purePerms->getGroup($values[1]);
+                if($this->getData()->getGroup($player) === $group){
+                    $player->sendMessage($this->replacePlaceholders($this->lang["Current-Rank"], $values[0]));
+                    return;
+                }
+                $this->purePerms->setGroup($player, $group);
+            } else{
+                $cmdString = $this->replacePlaceholders($this->config->getNested("Group.Command-String"), $values[0], $name);
+                $consolecmd = new ConsoleCommandSender();
+                $this->getServer()->getCommandMap()->dispatch($consolecmd, $cmdString);
+            }
+            $playerString = $this->replacePlaceholders($this->lang["Player-Report"], $values[0]);
+            $player->sendMessage($playerString);
+            if($this->config->get("Report-Rank-Change")){
+                $consoleString = $this->replacePlaceholders($this->lang["Console-Report"], $values[0], $name);
+                $this->getLogger()->info($consoleString);
+            }
+        } else{
+            $player->sendMessage($this->replacePlaceholders($this->lang["No-Rank"], $values[0]));
+        }
+        return;
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-        if(startsWith($command->getName(),"mma")){
+        if ($command->getName() == "makemeadmin"){
+            $sender->sendMessage(TF::GRAY."---".TF::GOLD." MakeMeAdmin ".TF::GRAY."---");
+            $sender->sendMessage(TF::YELLOW."Version: ".TF::AQUA.$this->pluginVersion);
+            $sender->sendMessage(TF::YELLOW."Description: ".TF::AQUA."Quickly switch your PurePerms group with a UI");
+            $sender->sendMessage(TF::YELLOW."Command: ".TF::BLUE."/".$this->cmdName);
+            $sender->sendMessage(TF::YELLOW."Ranks: ");
+            foreach($this->ranks as $rank){
+                $values = explode(":", $rank);
+                $values = str_replace("&", "§", $values);
+                $sender->sendMessage(TF::LIGHT_PURPLE ." - ".TF::GREEN.$values[0].TF::RESET.TF::GRAY." (Alias: ".$values[1].")");
+            }
+            $sender->sendMessage(TF::GRAY."-------------------");
+        } elseif($command->getName() == $this->cmdName){
             if($sender instanceof Player){
-                $canSwitch = FALSE;
+                $canSwitch = false;
                 foreach($this->ranks as $rank){
-                    $value = explode(":", $rank);
-                    if($sender->hasPermission($value[2]) || $sender->checkOP()){
-                        $canSwitch = TRUE;
+                    $values = explode(":", $rank);
+                    if($this->hasPerms($sender, $values[2])){
+                        $canSwitch = true;
                     }
                 }
-                if($canSwitch == FALSE){
-                    $sender->sendMessage(TF::RED."You don't have permission to switch ranks!");
-                }else{
-                    if($command->getName() == 'mma'){
+                if($canSwitch == false){
+                    $sender->sendMessage($this->lang["No-Switch"]);
+                } else{
+                    if($args == null){
                         $this->rankOptions($sender);
-                    }else{
-                        if($args[0] in_array($this->ranks){
-                            $prefix = $this->config->get("Group_Command");
-                            $name = $player->getName();
-                            $consolecmd = new ConsoleCommandSender();
-                            $value = explode(":", $this->ranks[$args[0]);
-                            $value = str_replace("&", "§", $value);
-                            if($player->hasPermission($value[2]) || $sender->checkOP()){
-                                $this->getServer()->getCommandMap()->dispatch($consolecmd, $prefix.' '.$name.' '.$value[1]);
-                                $player->sendMessage(TF::GREEN."You selected ".TF::YELLOW.$value[0].TF::RESET.TF::GREEN." as your rank!");
-                                $this->getLogger()->notice(TF::YELLOW.$name." has changed their group to ".$value[0]);
-                            }else{
-                                $player->sendMessage(TF::RED."You don't have permission to switch to this rank!");
-                                return;
+                    } else{
+                        foreach($this->ranks as $rank){
+                            $values = explode(":", $rank);
+                            if($this->purePerms->getGroup($values[1])->getAlias() == $args[0]){
+                                $this->setPlayerGroup($sender, $values);
+                                return true;
                             }
-                        }else{
-                            $player->sendMessage(TF::RED."The rank specified doesn't exist!");
-                            return;
                         }
+                        $sender->sendMessage($this->replacePlaceholders($this->lang["No-Exists"], $args[0]));
                     }
-                }                       
-            }else{
+                }
+            } else{
                 $sender->sendMessage(TF::RED."This is an in-game command only!");
             }
         }
@@ -118,42 +227,31 @@ class MakeMeAdmin extends PluginBase implements Listener{
         $form = new SimpleForm(function (Player $player, $data){
             if ($data === null){
                 return;
-            }else{
-                $prefix = $this->config->get("Group_Command");
-                $name = $player->getName();
-                $consolecmd = new ConsoleCommandSender();
-                $value = explode(":", $this->ranks[$data]);
-                $value = str_replace("&", "§", $value);
-                if($player->hasPermission($value[2]) || $sender->checkOP()){
-                    $this->getServer()->getCommandMap()->dispatch($consolecmd, $prefix.' '.$name.' '.$value[1]);
-                    $player->sendMessage(TF::GREEN."You selected ".TF::YELLOW.$value[0].TF::RESET.TF::GREEN." as your rank!");
-                    $this->getLogger()->notice(TF::YELLOW.$name." has changed their group to ".$value[0]);  
-                }else{
-                    $player->sendMessage(TF::RED."You don't have permission to switch to this rank!");
-                    return;
-                }
+            } else{
+                $values = explode(":", $this->ranks[$data]);
+                $this->setPlayerGroup($player, $values);
             }
-            return true;
+            return;
         });
-        $form->setTitle(TF::GOLD."Server Ranks");
-        $form->setContent(TF::GREEN."Pick the rank to switch to!");
+        $form->setTitle($this->config->getNested("UI.Title"));
+        $form->setContent($this->config->getNested("UI.Message"));
         foreach ($this->ranks as $rank) {
-            $value = explode(":", $rank);
-            $value = str_replace("&", "§", $value);
-            if($player->hasPermission($value[2]) || $sender->checkOP()){
-                if(isset($value[3])){
-                    if($value[3] == "url"){
-                        $form->addButton($value[0], 1, "https://".$value[4]);
+            $values = explode(":", $rank);
+            $values = str_replace("&", "§", $values);
+            if($this->hasPerms($player, $values[2])){
+                if(isset($values[3])){
+                    if($values[3] == "url"){
+                        $form->addButton($values[0], 1, "https://".$values[4]);
                     }
-                    if($value[3] == "path"){
-                        $form->addButton($value[0], 0, $value[4]);
+                    if($values[3] == "path"){
+                        $form->addButton($values[0], 0, $values[4]);
                     }
-                }else{
-                    $form->addButton($value[0]);
+                } else{
+                    $form->addButton($values[0]);
                 }
-            }else{
-                $value[0] = TF::clean($value[0]);
-                $form->addButton(TF::RESET.TF::DARK_GRAY.$value[0]." (Locked)");
+            } else{
+                $values[0] = TF::clean($values[0]);
+                $form->addButton(TF::RESET.TF::DARK_GRAY.$values[0]." (Locked)");
             }
         }
         $form->sendToPlayer($player);
